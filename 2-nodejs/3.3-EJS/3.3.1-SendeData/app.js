b804c6d2c72d3e0dd310863f960f9da4a8b68e66
kej83 <kej83@pm.me>
2025-10-24 10:10:54 +0200

    update

--- 2-nodejs/3.3-EJS/3.3.1-SendeData/app.js
+++ 2-nodejs/3.3-EJS/3.3.1-SendeData/app.js
@@ -6,7 +6,9 @@ const port = 3000;
 app.get("/", (req, res) => {
     let frukt = "eple";
     let data = {
-        minFrukt:frukt
+        minFrukt:frukt,
+        venner:["jens","per","ea"],
+        lydfiler: ["koko.wav","yo.mp3","mornajens.wav","donald.mp3"]
     }
     res.render("index.ejs", data);
 });
