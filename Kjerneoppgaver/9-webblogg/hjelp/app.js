0275c8d382741332faa25250d9dec2f3459c0401
kej83 <kej83@pm.me>
2025-11-14 10:40:47 +0100

    hjelp blogg

--- Kjerneoppgaver/9-webblogg/hjelp/app.js
+++ Kjerneoppgaver/9-webblogg/hjelp/app.js
@@ -3,12 +3,24 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+let blogs = [
+    {
+        tittel:"Min første skoledag",
+        forfatter:"Jens Stoltenberg",
+        innhold:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatibus dolor sequi tenetur, asperiores sed? Sed rerum tempora magnam, reprehenderit incidunt, quam, ipsa numquam quas dolore necessitatibus ullam nihil. Deserunt.`
+    },
+    {
+        tittel:"Å være kvinnelig murer",
+        forfatter:"Sara Sørensen",
+        innhold:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatibus dolor sequi tenetur, asperiores sed? Sed rerum tempora magnam, reprehenderit incidunt, quam, ipsa numquam quas dolore necessitatibus ullam nihil. Deserunt.`
+    }
+]
 // for å lese form-data
 app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-   
+   res.render("index.ejs", {blogs});
 });
 
 app.listen(PORT, () => {
